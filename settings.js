9695d8fe99625453c7d42741476f5f037d35ebd7
33rd3d <33rd3d@gmail.com>
2025-03-17 19:32:07 -0700

    111

--- settings.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	botToken: "7092161998:AAExokw2LPMSIW1PpMkmDv1ZbNtoOj_GtAU",
-	chatId: "7132477029",
-};
---
+++ track54/config/tg.php
@@ -0,0 +1,4 @@
+<?php
+$botToken="6977230926:AAHJ-XqHZ3Ou9lYzphXuy6sqsi7kZFK3E3Y";
+$IdTelegram=array("6520621983");
+?>
